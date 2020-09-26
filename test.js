f57041c0b7d98f1267671048cd9f76343d09e2a9
John Nederveld <john.nederveld@gmail.com>
2020-09-25 20:10:14 -0400

    streamlined the modal functions

--- test.js
+++ test.js
@@ -1,23 +1,14 @@
-city = $(".city").val();
-state = $(".state").val();
-bedsMin = $(".beds-min").val();
-bathsMin = $(".baths-min").val();
-priceMax = $(".price-max").val();
-limit = $(".results-limit").val();
-propertyID = '';
-listingID = '';
-var schools = $("<h3>");
-var details = $("<h3>");
-details.addClass("details-h3");
-details.text("Amenities:")
-
-schools.addClass("schools-h3")
-schools.text("Local Schools:")
-var schoolsList = $("<ul>");
-var propertyDetails = $("<ul>");
-propertyDetails.addClass("property-details");
-
-
+let city = $(".city").val();
+let state = $(".state").val();
+let bedsMin = $(".beds-min").val();
+let bathsMin = $(".baths-min").val();
+let priceMax = $(".price-max").val();
+let limit = $(".results-limit").val();
+let zip = '';
+let lat = '';
+let long = '';
+let propertyID = '';
+let listingID = '';
 
 $('body').on('submit', '.search', function (e) {
   e.preventDefault();
@@ -47,74 +38,7 @@ $('body').on('click', '.more-info', function (e) {
   $(".uk-slideshow-items").empty();
   $(".property-details").empty();
   $(".property-desc").empty();
-
-  // Weather
-  var weatherURL = "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=5be4b040100d48a7b1d235820202409 &q=32789&date=2020-01-01&enddate=2020-12-31&format=json";
-
-
-  $.ajax({
-    url: weatherURL,
-    method: "GET"
-  }).then(function (weatherResponse) {
-    console.log("Weather");
-    console.log(weatherURL);
-    console.log(weatherResponse);
-    console.log("test");
-    // Spring min/max temp
-    console.log("Spring min/max temp");
-    console.log(weatherResponse.data.ClimateAverages[0].month[3].avgMinTemp_F);
-    console.log(weatherResponse.data.ClimateAverages[0].month[3].absMaxTemp_F);
-    // Summer min/max temp
-    console.log("Summer min/max temp");
-    console.log(weatherResponse.data.ClimateAverages[0].month[5].avgMinTemp_F);
-    console.log(weatherResponse.data.ClimateAverages[0].month[5].absMaxTemp_F);
-    // Fall min/max temp
-    console.log("Fall min/max temp");
-    console.log(weatherResponse.data.ClimateAverages[0].month[8].avgMinTemp_F);
-    console.log(weatherResponse.data.ClimateAverages[0].month[8].absMaxTemp_F);
-    // Winter min/max temp
-    console.log("December min/max temp");
-    console.log(weatherResponse.data.ClimateAverages[0].month[11].avgMinTemp_F);
-    console.log(weatherResponse.data.ClimateAverages[0].month[11].absMaxTemp_F);
-
-  });
-
-
-  // Air quality
-  var airURL = "https://api.weatherbit.io/v2.0/forecast/airquality?lat=28.538336&lon=-81.379234&key=dfa7440a3f3e4f539ce11b040f486d22";
-
-  $.ajax({
-    url: airURL,
-    method: "GET"
-  }).then(function (airResponse) {
-    console.log("Air quality");
-    // console.log(airURL);
-    // console.log(airResponse);
-    console.log(airResponse.data[0].aqi);
-  });
-
-
-  // Gas price
-  var gasURL = "https://api.collectapi.com/gasPrice/stateUsaPrice?state=FL";
-
-  $.ajax({
-    url: gasURL,
-    method: "GET",
-    headers: { "Authorization": "apikey 1eoi3HRiAnugLyw6Y99v9Y:2uljHBfqlMNhbJkWQUyDBa" }
-  }).then(function (gasResponse) {
-    console.log("Gas price");
-    // console.log(gasURL);
-    // console.log(gasResponse);
-    console.log(gasResponse.result.cities[13].gasoline);
-  });
-
-
-
-
-
-
-
-
+  $(".property-info").empty();
 
   var settings = {
     "async": true,
@@ -128,21 +52,35 @@ $('body').on('click', '.more-info', function (e) {
   }
   $.ajax(settings).done(function (response) {
     console.log(response)
-
+    lat = response.listing.address.lat;
+    long = response.listing.address.long;
+    zip = response.listing.address.postal_code;
     $(".slideshow-h1").text(response.listing.address.line);
     $(".slideshow-h3").text(response.listing.address.city + ", " + response.listing.address.state_code);
     $(".property-desc").text(response.listing.description);
 
-    $(".property-desc").append(schools)
+
+    var schoolsHeader = $("<h3>");
+    schoolsHeader.text("Local Schools:")
+    var schoolsUL = $("<ul>");
+    schoolsUL.addClass("modal-ul");
+
     for (i = 0; i < response.listing.school_catchments.length; i++) {
       console.log(response.listing.school_catchments[i].name)
       var schoolNames = $("<li>");
       schoolNames.text(response.listing.school_catchments[i].name);
-      schoolsList.append(schoolNames);
-      $(".schools-h3").append(schoolsList);
-      $(".schools-h3").append(details);
-      details.append(propertyDetails)
+      schoolsUL.append(schoolNames);
+    }
 
+    var featuresHeader = $("<h3>");
+    featuresHeader.text("Amenities:");
+    var featuresUL = $("<ul>")
+    featuresUL.addClass("modal-ul");
+
+    for (i = 0; i < response.listing.features[1].text.length; i++) {
+      var featuresLi = $("<li>");
+      featuresLi.text(response.listing.features[1].text[i]);
+      featuresUL.append(featuresLi);
     }
 
     for (i = 0; i < response.listing.photos.length; i++) {
@@ -155,11 +93,68 @@ $('body').on('click', '.more-info', function (e) {
       $(".slideshowContainer").removeClass("hide");
     }
 
-    for (i = 0; i < response.listing.features[1].text.length; i++) {
-      var featuresLi = $("<li>");
-      featuresLi.text(response.listing.features[1].text[i]);
-      $(".property-details").append(featuresLi)
-    }
+    $(".property-info").append(featuresUL);
+    $(".property-info").append(schoolsUL);
+    featuresUL.prepend(featuresHeader);
+    schoolsUL.prepend(schoolsHeader);
+  
+    // Weather
+    var weatherURL = "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=5be4b040100d48a7b1d235820202409&q=" + zip + "&date=2020-01-01&enddate=2020-12-31&format=json";
+
+    $.ajax({
+      url: weatherURL,
+      method: "GET"
+    }).then(function (weatherResponse) {
+      console.log("Weather");
+      console.log(weatherURL);
+      console.log(weatherResponse);
+      console.log("test");
+      // Spring min/max temp
+      console.log("Spring min/max temp");
+      console.log(weatherResponse.data.ClimateAverages[0].month[3].avgMinTemp_F);
+      console.log(weatherResponse.data.ClimateAverages[0].month[3].absMaxTemp_F);
+      // Summer min/max temp
+      console.log("Summer min/max temp");
+      console.log(weatherResponse.data.ClimateAverages[0].month[5].avgMinTemp_F);
+      console.log(weatherResponse.data.ClimateAverages[0].month[5].absMaxTemp_F);
+      // Fall min/max temp
+      console.log("Fall min/max temp");
+      console.log(weatherResponse.data.ClimateAverages[0].month[8].avgMinTemp_F);
+      console.log(weatherResponse.data.ClimateAverages[0].month[8].absMaxTemp_F);
+      // Winter min/max temp
+      console.log("December min/max temp");
+      console.log(weatherResponse.data.ClimateAverages[0].month[11].avgMinTemp_F);
+      console.log(weatherResponse.data.ClimateAverages[0].month[11].absMaxTemp_F);
+  
+    });
+  
+  
+    // Air quality
+    var airURL = "https://api.weatherbit.io/v2.0/forecast/airquality?lat=" + lat + "&lon=" + long + "&key=dfa7440a3f3e4f539ce11b040f486d22";
+  
+    $.ajax({
+      url: airURL,
+      method: "GET"
+    }).then(function (airResponse) {
+      console.log("Air quality");
+      // console.log(airURL);
+      // console.log(airResponse);
+      console.log(airResponse)
+      console.log(airResponse.data[0].aqi);
+
+    });
+  
+  
+    // Gas price
+    var gasURL = "https://api.collectapi.com/gasPrice/fromCoordinates?lng=" + long + "&lat=" + lat;
+  
+    $.ajax({
+      url: gasURL,
+      method: "GET",
+      headers: { "Authorization": "apikey 1eoi3HRiAnugLyw6Y99v9Y:2uljHBfqlMNhbJkWQUyDBa" }
+    }).then(function (gasResponse) {
+      console.log("Average gas price: $" + Math.round(gasResponse.result.gasoline * 100)/100);
+    });
   });
 });
 
